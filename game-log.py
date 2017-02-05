57773d37b20285eba15cc78f4de4e3e344097624
Ansel Rosenberg <ansel01@gmail.com>
2017-02-04 23:49:13 -0800

    Add yahoo game log header parsing

--- game-log.py
+++ game-log.py
@@ -5,5 +5,23 @@ class YahooGameLog:
     def __init__(self, player_id):
         page = requests.get('http://sports.yahoo.com/nba/players/' + player_id + '/gamelog/')
         self.soup = BeautifulSoup(page.text, 'lxml')
+        self.column_names = self.get_headers()
+
+    def get_headers(self):
+        names = []
+        table = self.soup.find("table", attrs={"summary": "Player "})
+        headers = table.find('thead').find_all('tr')[1]
+
+        for header in headers:
+            if isinstance(header, Tag):
+                names.append(header.text)
+        return names
+
+    def columns(self):
+        return self.column_names
+
+
+game_log = YahooGameLog('4750')
+print(game_log.columns())
 
 
