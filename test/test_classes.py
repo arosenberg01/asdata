da73480f56ec8eba01d654093ee1f5d25131591e
Ansel Rosenberg <ansel01@gmail.com>
2017-06-16 21:21:50 -0400

    Add session calls tests for NbaTeamPage

--- test/test_classes.py
+++ test/test_classes.py
@@ -1,23 +1,18 @@
 import unittest
+from unittest.mock import Mock
 import os
 import gzip
-import bs4
 import logging
+import bs4
 from classes import (
-    NbaTeam
+    NbaTeamPage
 )
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
 
 class MockRequests:
     def get(self, url):
         pass
 
 class TestNbaTeamPage(unittest.TestCase):
-    # read html file and ungzip
-
     @classmethod
     def setUpClass(cls):
         file_path = os.path.join(os.path.dirname(__file__), 'mock_data/nba_roster_lakers.htm.gz')
@@ -26,8 +21,9 @@ class TestNbaTeamPage(unittest.TestCase):
 
     @classmethod
     def setUp(cls):
-        cls.nba_team = NbaTeam('okc', cls.requester, bs4)
+        cls.nba_team = NbaTeamPage('okc', cls.requester, bs4)
         cls.parsed = cls.nba_team.convert_page(cls.roster_text)
+        cls.session_mock = Mock()
 
     def test_get_page_should_not_fail(self):
         team_page = self.nba_team.get_page(self.nba_team.url)
@@ -42,6 +38,21 @@ class TestNbaTeamPage(unittest.TestCase):
             '5318', '5011', '5433', '3339', '4294', '5663']
         player_ids = self.nba_team.parse_roster(self.parsed)
         self.assertEqual(expected, player_ids)
+
+    def test_update_team_calls_session_methods(self):
+        self.nba_team.update_team(self.session_mock, '', [])
+        self.session_mock.query.assert_called_once()
+        self.session_mock.add.assert_called_once()
+
+# class TestNbaPlayerPage(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         pass
+
+#     @classmethod
+#     def setUp(cls):
+#         pass
+
 
 if __name__ == '__main__':
     unittest.main()
