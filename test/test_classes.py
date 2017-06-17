9a87e5fe854fd7a1efaa00d19ef035d16fe579cd
Ansel Rosenberg <ansel01@gmail.com>
2017-06-17 19:44:47 -0400

    Add NbaPlayerPage tests and mock data

--- test/test_classes.py
+++ test/test_classes.py
@@ -2,41 +2,34 @@ import unittest
 from unittest.mock import Mock
 import os
 import gzip
-import logging
 import bs4
 from classes import (
-    NbaTeamPage
+    NbaTeamPage,
+    NbaPlayerPage
 )
-
-class MockRequests:
-    def get(self, url):
-        pass
 
 class TestNbaTeamPage(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         file_path = os.path.join(os.path.dirname(__file__), 'mock_data/nba_roster_lakers.htm.gz')
-        cls.roster_text = gzip.open(file_path).read()
-        cls.requester = MockRequests()
+        cls.html = gzip.open(file_path).read()
 
     @classmethod
     def setUp(cls):
-        cls.nba_team = NbaTeamPage('okc', cls.requester, bs4)
-        cls.parsed = cls.nba_team.convert_page(cls.roster_text)
+        cls.request_mock = Mock()
+        cls.parse_mock = Mock()
         cls.session_mock = Mock()
+        cls.nba_team = NbaTeamPage('okc', cls.request_mock, bs4)
+        cls.soup = cls.nba_team.convert(cls.html)
 
-    def test_get_page_should_not_fail(self):
-        team_page = self.nba_team.get_page(self.nba_team.url)
-        self.assertFalse(self.nba_team.failed)
-
-    def test_convert_page_should_not_fail(self):
-        parsed_page = self.nba_team.convert_page(self.roster_text)
-        self.assertFalse(self.nba_team.failed)
+    def test_get_should_call_requester_get(self):
+        self.nba_team.get(self.nba_team.url)
+        self.request_mock.get.assert_called_once()
 
     def test_parse_roster_should_return_player_ids(self):
         expected = ['5383', '4285', '5357', '3824', '5329', '5601', '4794', '5487', '5762',
             '5318', '5011', '5433', '3339', '4294', '5663']
-        player_ids = self.nba_team.parse_roster(self.parsed)
+        player_ids = self.nba_team.parse_roster(self.soup)
         self.assertEqual(expected, player_ids)
 
     def test_update_team_calls_session_methods(self):
@@ -44,15 +37,33 @@ class TestNbaTeamPage(unittest.TestCase):
         self.session_mock.query.assert_called_once()
         self.session_mock.add.assert_called_once()
 
-# class TestNbaPlayerPage(unittest.TestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#         pass
+class TestNbaPlayerPage(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        file_path = os.path.join(os.path.dirname(__file__), 'mock_data/nba_player_ingram.htm.gz')
+        cls.html = gzip.open(file_path).read()
 
-#     @classmethod
-#     def setUp(cls):
-#         pass
+    @classmethod
+    def setUp(cls):
+        cls.request_mock = Mock()
+        cls.parse_mock = Mock()
+        cls.session_mock = Mock()
+        cls.nba_player = NbaPlayerPage('5601', cls.request_mock, bs4)
+        cls.soup = cls.nba_player.convert(cls.html)
 
+    def test_get_should_call_requester_get(self):
+        self.nba_player.get(self.nba_player.url)
+        self.request_mock.get.assert_called_once()
+
+    def test_games_returns_game_log(self):
+        expected_columns = ['Date', 'Opp', 'Score', 'Type', 'Min', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%', 'FTM',
+                            'FTA', 'FT%', 'OR', 'DR', 'Reb', 'Ast', 'TO', 'Stl', 'Blk', 'PF', 'Pts']
+        expected_first_row = [None, '@GS', 'L 94-109', 'Reg', '29:26', '5', '16', '31.3', '0', '0', '-', '1', '2',
+                              '50.0', '1', '4', '5', '5', '5', '3', '0', '2', '11']
+
+        games = self.nba_player.parse_games(self.soup)
+        self.assertListEqual(expected_columns, games['columns'])
+        self.assertListEqual(expected_first_row, games['rows'][0])
 
 if __name__ == '__main__':
     unittest.main()
